58481deefe5e6e68443c6279a5fffbb24f5b822c
CapelleGab <gabin.capelle.pro@gmail.com>
2025-03-18 22:55:59 +0100

    📦 NEW : Add default conversion folder setting

---
+++ src/configs/settings.ts
@@ -0,0 +1,39 @@
+import { App, PluginSettingTab, Setting } from "obsidian";
+import PDFtoMD from "../../main";
+
+export interface PDFtoMDSettings {
+	defaultFolder: string;
+}
+
+export const DEFAULT_SETTINGS: PDFtoMDSettings = {
+	defaultFolder: "/Templates/",
+};
+
+export class PDFtoMDSettingTab extends PluginSettingTab {
+	plugin: PDFtoMD;
+
+	constructor(app: App, plugin: PDFtoMD) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const { containerEl } = this;
+
+		containerEl.empty();
+		new Setting(containerEl)
+			.setName("Default conversion folder")
+			.setDesc(
+				"Select the folder where converted Markdown files will be saved."
+			)
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter folder path")
+					.setValue(this.plugin.settings.defaultFolder)
+					.onChange(async (value) => {
+						this.plugin.settings.defaultFolder = value;
+						await this.plugin.saveSettings();
+					})
+			);
+	}
+}
