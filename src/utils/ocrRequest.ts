678e5c0bfb630ba33ac1187d86accd7b7b53d6f7
CapelleGab <gabin.capelle.pro@gmail.com>
2025-03-18 22:57:01 +0100

    📦 NEW: All action for MISTRAL OCR

---
+++ src/utils/ocrRequest.ts
@@ -0,0 +1,35 @@
+import {Mistral} from "@mistralai/mistralai";
+import {FileSignedURL, UploadFileOut} from "@mistralai/mistralai/models/components";
+
+
+const apiKey = "hRf3KcdkFoHXoOQyaSjMxFbb3iztuuv0";
+const client = new Mistral({ apiKey: apiKey });
+
+export const uploadPDFtoMistral= async (file: File) => {
+	return await client.files.upload({
+		file: {
+			fileName: file.name,
+			content: file,
+		},
+		purpose: "ocr",
+	});
+}
+
+export const getSignedUrl = async (uploaded_PDF_Id: UploadFileOut) => {
+	return await client.files.getSignedUrl({
+		fileId: uploaded_PDF_Id.id,
+	});
+}
+
+export const getJsonFromSignedUrl = async (signedUrl: FileSignedURL) => {
+	return await client.ocr.process({
+		model: "mistral-ocr-latest",
+		document: {
+			type: "document_url",
+			documentUrl: signedUrl.url
+		},
+		includeImageBase64: true,
+	});
+
+}
+
