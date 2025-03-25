027bc06be3e6ffae4c0adb03704651aead3783ad
CapelleGab <gabin.capelle.pro@gmail.com>
2025-03-25 20:21:06 +0100

    📦 NEW: Add Right click menu (#8)

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { Notice, Plugin } from "obsidian";
+import {Menu, MenuItem, Notice, Plugin, TFile} from "obsidian";
 import {
 	DEFAULT_SETTINGS,
 	PDFtoMDSettings,
@@ -10,10 +10,11 @@ export default class PDFtoMD extends Plugin {
 	settings: PDFtoMDSettings;
 
 	async onload() {
-		// Load
+		// Load settings and styles
 		await this.loadSettings();
 		this.loadStyles();
 
+		// Add ribbon icon
 		this.addRibbonIcon(
 			"arrow-right-left",
 			"PDF to Markdown",
@@ -22,6 +23,7 @@ export default class PDFtoMD extends Plugin {
 			}
 		);
 
+		// Add command to trigger modal
 		this.addCommand({
 			id: "pdf-to-md",
 			name: "PDF To Markdown",
@@ -30,12 +32,43 @@ export default class PDFtoMD extends Plugin {
 			},
 		});
 
+		// Add setting tab for plugin settings
 		this.addSettingTab(new PDFtoMDSettingTab(this.app, this));
-		this.registerInterval(
-			window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000)
+
+		// Register right-click menu for PDF files
+		this.registerEvent(
+			this.app.workspace.on("file-menu", (menu: Menu, file: TFile) => {
+				if (file.extension === "pdf") {
+					menu.addItem((item: MenuItem) => {
+						item.setTitle("Extract file into a new note")
+							.setIcon("file-text")
+							.onClick(async () => {
+								const fileBlob = await this.getFileAsBlob(file);
+								if (!fileBlob) {
+									new Notice("Impossible de lire le fichier PDF.");
+									return;
+								}
+								const newFile = new File([fileBlob], file.name, { type: "application/pdf" });
+								new pdfToMdModal(this.app, this, newFile).open();
+							});
+					});
+				}
+			})
 		);
 	}
 
+	// Method to convert TFile to Blob
+	async getFileAsBlob(file: TFile): Promise<Blob | null> {
+		try {
+			const data = await this.app.vault.readBinary(file);
+			return new Blob([data], { type: "application/pdf" });
+		} catch (error) {
+			console.error("Erreur lors de la lecture du fichier :", error);
+			return null;
+		}
+	}
+
+	// Load styles for plugin
 	loadStyles() {
 		const link = document.createElement("link");
 		link.rel = "stylesheet";
@@ -45,10 +78,12 @@ export default class PDFtoMD extends Plugin {
 		document.head.appendChild(link);
 	}
 
+	// Unload plugin
 	onunload() {
 		return new Notice("PDF To MD - Unloaded");
 	}
 
+	// Load settings from file
 	async loadSettings() {
 		this.settings = Object.assign(
 			{},
@@ -57,6 +92,7 @@ export default class PDFtoMD extends Plugin {
 		);
 	}
 
+	// Save settings to file
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
